70431b5656210653e2b123bdea66edde99dafa66
Hassan Tahir <hassan.tahir@IntelliasPro.local>
2026-04-01 07:38:48 -0400

    migrations/first

---
+++ app/Models/patient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Console\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['firstName', 'email', 'lastName', 'middleName', 'phone', 'mobile', 'dateOfBirth', 'gender', 'address', 'city', 'state', 'postalCode', 'country', 'emergencyContactName', 'emergencyContactPhone'])]
+class patient extends Model
+{
+    public function getFullNameAttribute(): string
+    {
+        return collect([$this->first_name, $this->middle_name, $this->last_name])
+            ->filter()
+            ->implode(' ');
+    }
+}
