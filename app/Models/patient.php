06b9798a89e1eaf2b062d195054bce29705028d9
Hassan Tahir <hassan.tahir@IntelliasPro.local>
2026-04-02 02:06:51 -0400

    procedureCode

--- app/Models/patient.php
+++ app/Models/patient.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
-use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
 
 #[Table('patients')]
