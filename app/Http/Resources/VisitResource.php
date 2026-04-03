82ce1d9aa27772ae2195cc50628c8d5af207d359
Hassan Tahir <hassan.tahir@IntelliasPro.local>
2026-04-03 07:49:06 -0400

    last

---
+++ app/Http/Resources/VisitResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VisitResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"=> $this->appointment->case->patient->id,
+            "patient_name"=> $this->appointment->case->patient->name,
+            "case_type"=> $this->appointment->case->case_type,
+            "case_category"=> $this->appointment->case->case_category,
+            "is_accident"=> $this->appointment->case->is_accident,
+            "appointment_date"=> $this->appointment->appointment_date,
+            "appointment_time"=> $this->appointment->appointment_time,
+            "doctor_name"=> $this->appointment->doctor_name,
+            "diagnosis"=> $this->diagnosis
+        ];
+    }
+}
